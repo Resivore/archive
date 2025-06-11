6f8b4d3546f529aa99ffd82f52424d573ab58fe7
Remi Simpson <resivore@gmail.com>
2025-06-11 02:00:51 -0500

    fixes

--- _data/users.js
+++ _data/users.js
@@ -4,7 +4,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 module.exports = async () => {
   // these env vars you’ll set in Netlify (or your local .env):
-  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
   const supabase = createClient(supabaseUrl, supabaseKey);
 
